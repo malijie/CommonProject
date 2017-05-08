07953ea34eb787ffd66fc31ae66e8eeeb4a7e3e5
malijie <190223629@qq.com>
2017-05-08 19:27:58 +0800

    update umeng share sdk resource

--- app/src/main/java/com/vic/activity/CommonApplication.java
+++ app/src/main/java/com/vic/activity/CommonApplication.java
@@ -2,6 +2,8 @@ package com.vic.activity;
 
 import android.app.Application;
 
+import com.umeng.socialize.PlatformConfig;
+import com.umeng.socialize.UMShareAPI;
 import com.vic.base.profile.Profile;
 
 /**
@@ -13,5 +15,10 @@ public class CommonApplication extends Application{
     public void onCreate() {
         super.onCreate();
         Profile.init(getApplicationContext());
+        UMShareAPI.get(this);
+        PlatformConfig.setWeixin("wx967daebe835fbeac", "5bb696d9ccd75a38c8a0bfe0675559b3");
+        PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
+        PlatformConfig.setSinaWeibo("3921700954", "04b48b094faeb16683c32669824ebdad", "http://sns.whalecloud.com");
+
     }
 }
--- app/src/main/java/com/vic/activity/MainActivity.java
+++ app/src/main/java/com/vic/activity/MainActivity.java
@@ -4,10 +4,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.umeng.socialize.ShareAction;
+import com.umeng.socialize.UMShareListener;
+import com.umeng.socialize.bean.SHARE_MEDIA;
 import com.vic.base.core.ActionCallbackListener;
 import com.vic.base.entity.QuestionItem;
 import com.vic.base.http.HttpBase;
 import com.vic.base.util.Logger;
+import com.vic.base.util.ToastManager;
 
 import java.util.HashMap;
 import java.util.List;
@@ -18,22 +22,57 @@ import cameraclient.activity.R;
 
 public class MainActivity extends BaseActivity {
     private Button mButton = null;
-
+    private Button mButtonShare = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mButton = (Button) findViewById(R.id.id_button);
+        mButtonShare = (Button) findViewById(R.id.id_button_share);
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 getDeviceList();
             }
         });
-
+        mButtonShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new ShareAction(MainActivity.this).withText("hello")
+                        .setDisplayList(SHARE_MEDIA.SINA,SHARE_MEDIA.QQ,SHARE_MEDIA.WEIXIN)
+                        .setCallback(umShareListener).open();
+            }
+        });
 
     }
+
+    private UMShareListener umShareListener = new UMShareListener() {
+        @Override
+        public void onStart(SHARE_MEDIA platform) {
+            //分享开始的回调
+        }
+        @Override
+        public void onResult(SHARE_MEDIA platform) {
+            Logger.mlj("platform"+platform);
+
+            ToastManager.showLongMsg(platform + " 分享成功啦");
+
+        }
+
+        @Override
+        public void onError(SHARE_MEDIA platform, Throwable t) {
+            ToastManager.showLongMsg(platform + " 分享失败啦");
+            if(t!=null){
+                Logger.mlj("throw:"+t.getMessage());
+            }
+        }
+
+        @Override
+        public void onCancel(SHARE_MEDIA platform) {
+            ToastManager.showLongMsg(platform + " 分享取消了");
+        }
+    };
 
     private void getDeviceList(){
         Map<String,String> paramsMap = new HashMap<>();
